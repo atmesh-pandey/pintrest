b0f59bdb9c86b8ffe09509e93dc25b1797e7b2c2
Atmesh Pandey <myattitude855@gmail.com>
2024-11-18 15:24:51 +0530

    cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const connectToDatabase = require("./services/mongoDB.service")
 const indexRouter = require('./routes/index');
 const usersRouter = require('./model/users');
+const cors = require('cors');
 require("dotenv").config();
 
 const app = express();
@@ -11,6 +12,13 @@ connectToDatabase();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors({
+  origin: '*',  // Allow all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true  // Allow cookies if needed
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
