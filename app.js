03b7d01cf6513a5501416147a89b33b1aacc7c28
Atmesh Pandey <myattitude855@gmail.com>
2024-11-24 15:48:11 +0530

    fix

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ require("dotenv").config();
 const app = express();
 connectToDatabase();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors({ origin: RegExp(process.env.CORS_URL, "i"), credentials: true }));
